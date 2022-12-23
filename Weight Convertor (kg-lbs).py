ee01e839f137c53c9bc463c82e6c981bda5e5718
Li Foong Chan <121106021+lifoongchan@users.noreply.github.com>
2022-12-23 14:20:35 +0000

    Delete Weight Convertor (kg-lbs).py

--- Weight Convertor (kg-lbs).py
+++
@@ -1,9 +0,0 @@
-w = float(input("Enter your weight: "))
-u = input("(L)bs or (K)g: ").upper()
-x = 0.45359237
-
-if u == "L":
-     print(f"your weight in kg is {round(w*x,2)}")
-
-if u == "K":
-     print(f"your weight in lbs is {round(w/x,2)}")
